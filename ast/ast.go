c909181ca4910b0ba868decc0a145f6e65e72027
Bhishan Bhandari <bbhishan@gmail.com>
2018-09-22 18:47:09 +0545

    A general AST requirements

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/bhishan/MonkeyInterpreter/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -28,3 +30,34 @@ func (p *Program) TokenLiteral() string{
 	}
 }
 
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+
+func (ls *LetStatement) statementNode(){
+
+}
+
+
+func (ls *LetStatement) TokenLiteral() string{
+	return ls.Token.Literal
+}
+
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+
+func (i *Identifier) expressionNode(){
+
+}
+
+
+func (i *Identifier) TokenLiteral() string{
+	return i.Token.Literal
+}
